c33fd90c0ee6f38ae11185366de37303d8551fc7
swused <69177074+swused@users.noreply.github.com>
2025-04-11 22:00:35 +0500

    Add files via upload

---
+++ verstka-2/js/main.js
@@ -0,0 +1,26 @@
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header-active');
+        } else {
+            header.classList.remove('header-active');
+        }
+    };
+}
+
+    ());
+
+// burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header-nav');
+    const menuCloseItem = document.querySelector('.header-nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header-nav-active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header-nav-active');
+    })
+}());
